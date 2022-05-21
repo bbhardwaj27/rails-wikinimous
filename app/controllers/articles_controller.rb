00eec5b17aad525c32f8e8cbfaf75f224133049b
Bhawana Bhardwaj <b.bhardwaj@bluewin.ch>
2022-05-21 20:00:06 +0200

    index is working

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,26 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
